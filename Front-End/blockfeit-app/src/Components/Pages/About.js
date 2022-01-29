07f5458a91a4a830f98f2a05201003814da660ed
Ritz <dell@DESKTOP-001DS0E.com>
2022-01-29 18:39:05 +0530

    About us contact us page updated

--- Front-End/blockfeit-app/src/Components/Pages/About.js
+++ Front-End/blockfeit-app/src/Components/Pages/About.js
@@ -1,9 +1,97 @@
-import React from "react";
+/*import React from "react";
 function About() {
   return (
     <div className="start">
-      <h1> About </h1>
-    </div>
+      
+    <h1> About us </h1>
+    <p>Hello , Wishes from Our Team</p>
+    <br/>
+
+    <p className="tagline  offset-lg-2 " >
+    In order to ensure the identification and trace-ability of genuine<br/>
+    products throughout the supply chain, we plan to implement a fully <br/>
+    functional block-chain application system to prevent product counterfeiting.
+    </p>
+    <br/>
+   
+    
+     
+    
+</div>
+
+
+
   );
 }
-export default About;
+export default About;*/
+
+
+
+import React from 'react';
+import '../../style/About.css';
+
+function Contact(){
+    return(
+<div className="maincontainer">
+<div class="container-fluid">
+    <div class="row no-gutter">
+       
+        <div class="col-md-6 d-none d-md-flex bg-image"></div>
+        
+        <div class="col-md-6 bg-light">
+            <div class="login d-flex align-items-center py-8">
+               
+                <div class="container">
+                    <div class="row-10 col-30">
+                        <div class="w3-panel w3-padding-16">
+                        
+                            <h1 class="h1">About us</h1>
+                            
+                          <div class="h4">
+
+                          <h4>
+                            
+    In order to ensure the identification and trace-ability of genuine
+    products throughout the supply chain, we plan to implement a fully 
+    functional block-chain application system to prevent product counterfeiting.
+                            </h4>
+                            <br/>
+
+                            <h4>
+                            We describe a Block-chain Web Application System with products
+                            anti-counterfeiting with an additional smart contract feature
+                            that enhances trust between the customer and manufacturer
+                            </h4>
+                            <br/>
+
+
+                            <h2 class="p1"> Our Team</h2><br/>
+                            <div class="p2">
+                              <h6>1) Ritesh Borse</h6>
+                              <h6>2) Payal Thakur</h6>
+                              <h6>3) Chandrakant Shinde</h6>
+                              <h6>4) Kalyani Sonawane</h6>
+                            </div>
+                              
+
+                            </div>
+
+                          
+                             
+                            
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+
+);
+}
+
+export default Contact;
+
+
--- Front-End/blockfeit-app/src/Components/Pages/Contact.js
+++ Front-End/blockfeit-app/src/Components/Pages/Contact.js
@@ -1,9 +1,60 @@
-import React from "react";
-function Contact() {
-  return (
-    <div className="start">
-      <h1> Contact </h1>
+
+import React from 'react';
+import '../../style/Contact.css';
+
+function Contact(){
+    return(
+<div className="maincontainer">
+<div class="container-fluid">
+    <div class="row no-gutter">
+       
+        <div class="col-md-6 d-none d-md-flex bg-image"></div>
+        
+        <div class="col-md-6 bg-light">
+            <div class="login d-flex align-items-center py-5">
+               
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-10 col-xl-7 mx-auto">
+                        
+                            <p class="text-muted mb-4"> <h3>Get In Touch</h3> </p>
+                            <form>
+
+                               <div class="mb-3">
+                                <h6> Name</h6>
+                                    <input id="inputUserid" type="userid" placeholder="" required="" autofocus="" class="form-control  border-0 shadow-sm px-4" />
+                                </div>
+
+                                <div class="mb-3">
+                                <h6> Email ID</h6>
+                                    <input id="inputUserid" type="userid" placeholder="" required="" autofocus="" class="form-control border-0 shadow-sm px-4" />
+                                </div>
+                                
+                                <div class="mb-3">
+                                <h6> Message</h6>
+                                    <input id="inputmessage" type="password" placeholder="" required=""  class="form-control  border-0 shadow-sm px-4 text-primary" />
+                                </div>
+
+                                
+                
+                                <div class="d-grid gap-2 mt-2">
+                                <button type="submit" class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm"> Submit</button>
+                                </div>
+                                
+                               
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  );
+</div>
+</div>
+
+);
 }
+
 export default Contact;
+
