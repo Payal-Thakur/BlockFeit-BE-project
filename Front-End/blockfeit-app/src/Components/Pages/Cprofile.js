604988934e9c46a843a30625c5a71847bacf736e
Ritz <dell@DESKTOP-001DS0E.com>
2022-02-02 17:50:22 +0530

    customer page created

--- Front-End/blockfeit-app/src/Components/Pages/Cprofile.js
+++ Front-End/blockfeit-app/src/Components/Pages/Cprofile.js
@@ -1,6 +1,87 @@
 import React  from "react";
 import '../../style/Cprofile.css';
-import{ useNavigate } from "react-router-dom";
+
+
+function Cprofile()
+{
+    return (
+ <div> 
+        <div class="container">
+  <div class="row">
+    <div class="col-lg-4 shadow-sm px-4">
+      
+      
+      <div class="box">
+       </div>
+       <br></br>
+
+       <h1> Chandu Shinde</h1> 
+       <h4> Customer</h4>
+       <br></br>
+
+         <h5>Location :  Nashik</h5><br></br>
+         <h5>Email    :  abc@gmail.com</h5><br></br>
+         <h5>Contact  :  9548623217</h5><br></br>
+         <h5>Password :  123456</h5>
+
+        
+      
+
+    </div>
+
+    <div class="col-lg-8 shadow-sm px-4">
+       <h3 id ="k1"> keys</h3>
+       
+       <br></br>
+       <h4 class="p1">Private Key : </h4> 
+       <div class="box">
+       </div>
+      
+       
+       
+       <h4 class="p1">Public Key   : </h4>
+       <div class="box">
+       </div>
+      
+
+    </div>
+   
+  </div>
+
+  
+   
+</div>
+
+<div>
+<h3 id="tags"> My Own Product</h3> <br></br>
+<table id="alignment">
+    <tr>
+        <th>Product No</th>
+        <th>Product Details</th>
+    </tr>
+    <tr>
+        <td>
+            1
+        </td>
+        <td>sellername: ABC ,
+             Amount:600,
+             product name: shoes</td>
+    </tr>
+    
+</table>
+
+
+</div>
+
+
+
+</div>
+        
+    );
+}
+
+export default Cprofile;
+/*import{ useNavigate } from "react-router-dom";
 function Cprofile()
 {
     let navigate = useNavigate();
@@ -23,30 +104,47 @@ function Cprofile()
                         
                             <p class="text-muted mb-4"> <h3>Its Customer's Profile</h3> </p>
                             <form>
+                            <div class="mb-3">
+                                <h6> Name</h6>
+                                   <div class="box">
+                                       
+
+                                   </div>
+                                </div>
+
                                 <div class="mb-3">
                                 <h6> Email ID</h6>
-                                    <input id="inputUserid" type="userid" placeholder="" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-2" />
+                                <div class="box">
+
+                                    </div>
                                 </div>
                                 
                                 <div class="mb-3">
                                 <h6> Password</h6>
-                                    <input id="inputPassword" type="password" placeholder="" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
+                                <div class="box">
+
+                                    </div>
                                 </div>
 
                                 <div class="mb-3">
-                                <h6> User Type</h6>
-                                <input id="inputPassword" type="password" placeholder="" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary" />
-                                    
+                                <h6>User Type</h6>
+                                <div class="box">
+
+                                 </div>
                                 </div>
 
                                 <div class="mb-3">
-                                <h6> Public Key</h6>
-                                    <input id="inputUserid" type="userid" placeholder="" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+                                <h6>Private Key</h6>
+                                <div class="box">
+
+                              </div>
                                 </div>
                                 
                                 <div class="mb-3">
-                                <h6> Private Key</h6>
-                                    <input id="inputUserid" type="userid" placeholder="" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4" />
+                                <h6> Public Key </h6>
+                                <div class="box">
+
+                                </div>
                                 </div>
                                 
                 
@@ -68,4 +166,4 @@ function Cprofile()
 }
 
 
-export default Cprofile;
+export default Cprofile;*/
